8b0b4266844704eed4791a03778765a031b8806d
Tony <1392997026@qq.com>
2018-07-29 18:17:08 +0800

    main

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	println("-----------")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"msg": "pong",
+		})
+	})
+	r.Run(":3000")
 }
